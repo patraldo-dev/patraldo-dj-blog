1b1ca61bc46552b6250586ab8c1a695e89a34cfd
Chef <cheftech@patraldo.com>
2024-10-05 15:34:09 -0600

    -

--- src/patraldo/settings.py
+++ src/patraldo/settings.py
@@ -47,9 +47,9 @@ DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY", default=get_random_secret_key())  # <-- Updated!
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.src-broken-dew-5673.fly.dev', 'patraldo.dev'] 
 
-#CSRF_TRUSTED_ORIGINS = ['https://*.fly.dev']  # <-- Updated!
+CSRF_TRUSTED_ORIGINS = ['https://src-broken-dew-5673.fly.dev']  # <-- Updated!
 
 
 
